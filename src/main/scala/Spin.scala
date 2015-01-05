3819514c729537020cda0979a1b7f35e02b19261
softprops <d.tangren@gmail.com>
2015-01-04 23:46:27 -0500

    progress

--- src/main/scala/Spin.scala
+++ src/main/scala/Spin.scala
@@ -1,21 +1,23 @@
 package spinster
 
 object Spin {
-  val Boxes0 = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
-  val Boxes1 = "⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓"
-  val Boxes2 = "⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆"
-  val Boxes3 = "⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋"
-  val Boxes4 = "⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁"
-  val Boxes5 = "⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈"
-  val Boxes6 = "⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈"
-  val Clock = "◴◷◶◵"
-  val Rotate = "╫╪"
+  val Boxes0 = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏".split("")
+  val Boxes1 = "⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓".split("")
+  val Boxes2 = "⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆".split("")
+  val Boxes3 = "⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋".split("")
+  val Boxes4 = "⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁".split("")
+  val Boxes5 = "⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈".split("")
+  val Boxes6 = "⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈".split("")
+  val Clock = "◴◷◶◵".split("")
+  val Rotate = "╫╪".split("")
+  val Progress = Array("o..", ".o.", "..o")
 }
 
 // for (_ <- 0 to 30) { Thread.sleep(100); print(s"\r\033[36m${s.next}\033[0m") }
-case class Spin(it: Iterable[Char] = Spin.Boxes0) {
+case class Spin(it: Iterable[String] = Spin.Boxes0) extends Iterator[String] {
   @volatile private[this] var iter = it.iterator
-  def next(): Char = {
+  def hasNext = it.nonEmpty
+  def next(): String = {
     if (iter.isEmpty) {
       iter = it.iterator
     }
